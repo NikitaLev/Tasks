fe1c5c583f37792b922ea51edff1eccf09f5bc2b
NikitaLev <nikita.leventev97@gmail.com>
2023-08-11 13:25:12 +0300

    Added save links

--- lib/main.dart
+++ lib/main.dart
@@ -959,7 +959,7 @@ class _HubPage extends State<HubPage> {
   late List<String> ttt = <String>['testtt'];
   _HubPage({required hub}) {
     this.hub = hub;
-    print("test --- " + hub.name);
+    //print("test --- " + hub.name);
   }
   @override
   Widget build(BuildContext context) {
@@ -1170,7 +1170,13 @@ class _HubPage extends State<HubPage> {
                                 ),
                               ),
                             ),
-                            onPressed: () {},
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          Hub_add_link(hub: hub)));
+                            },
                             child: Row(
                               textDirection: TextDirection.ltr,
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -1288,7 +1294,6 @@ class _HubPage extends State<HubPage> {
   }
 
   _selectFile() async {
-    print("1111111111111");
     FilePickerResult? directory = await FilePicker.platform.pickFiles();
     if (directory != null) {
       setState(() {
@@ -1297,5 +1302,215 @@ class _HubPage extends State<HubPage> {
         HUB.saveHub(hub);
       });
     }
+  }
+}
+
+class Hub_add_link extends StatefulWidget {
+  late HUB hub;
+
+  Hub_add_link({super.key, required hub}) {
+    this.hub = hub;
+  }
+  @override
+  _Hub_add_link createState() => _Hub_add_link(hub: hub);
+}
+
+class _Hub_add_link extends State<Hub_add_link> {
+  late HUB hub;
+  late String src;
+  late List<String> ttt = <String>['testtt'];
+  TextEditingController controller = TextEditingController();
+  late String currentDate;
+
+  _Hub_add_link({required hub}) {
+    this.hub = hub;
+    //print("test --- " + hub.name);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    currentDate = now.toString();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              'Link list chenger',
+            ),
+            backgroundColor: Colors.green,
+          ),
+          body: Container(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                  flex: 5,
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    alignment: Alignment.centerLeft,
+                    child: Column(children: [
+                      Container(
+                          padding: EdgeInsets.only(top: 10, bottom: 10),
+                          alignment: Alignment.centerLeft,
+                          child: Text("Enter link:")),
+                      TextField(
+                        controller: controller,
+                        decoration: InputDecoration(
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  )),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: Row(children: [
+                    Expanded(
+                        flex: 1,
+                        child: Builder(builder: (BuildContext context) {
+                          return Container(
+                            padding: EdgeInsets.all(10),
+                            alignment: Alignment.bottomLeft,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.orange,
+                                padding: const EdgeInsets.all(16),
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15.0),
+                                  ),
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => main_widget()),
+                                );
+                              },
+                              child: Row(
+                                textDirection: TextDirection.ltr,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    flex: 4,
+                                    child: Container(
+                                        alignment: Alignment.centerRight,
+                                        child: RichText(
+                                            textDirection: TextDirection.ltr,
+                                            text: const TextSpan(
+                                              children: [
+                                                TextSpan(
+                                                  text: 'Cancel',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 16,
+                                                  ),
+                                                ),
+                                              ],
+                                            ))),
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Container(
+                                      padding: EdgeInsets.only(top: 4, left: 5),
+                                      alignment: Alignment.centerLeft,
+                                      child: const Icon(
+                                        Icons.cancel_outlined,
+                                        size: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        })),
+                    Expanded(
+                        flex: 1,
+                        child: Builder(builder: (BuildContext context) {
+                          return Container(
+                            padding: EdgeInsets.all(10),
+                            alignment: Alignment.bottomLeft,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.green,
+                                padding: const EdgeInsets.all(16),
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15.0),
+                                  ),
+                                ),
+                              ),
+                              onPressed: () {
+                                _add_link();
+                              },
+                              child: Row(
+                                textDirection: TextDirection.ltr,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    flex: 2,
+                                    child: Container(
+                                        alignment: Alignment.centerRight,
+                                        child: RichText(
+                                            textDirection: TextDirection.ltr,
+                                            text: const TextSpan(
+                                              children: [
+                                                TextSpan(
+                                                  text: 'Add',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 16,
+                                                  ),
+                                                ),
+                                                TextSpan(
+                                                  text: ' Link',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ],
+                                            ))),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      padding: EdgeInsets.only(top: 4, left: 5),
+                                      alignment: Alignment.centerLeft,
+                                      child: const Icon(
+                                        Icons.add_circle_outline_outlined,
+                                        size: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        })),
+                  ]),
+                ),
+              ),
+            ],
+          )),
+        ));
+  }
+
+  _add_link() async {
+    setState(() {
+      String link = controller.text;
+      hub.addLink(link);
+      HUB.saveHub(hub);
+    });
   }
 }
